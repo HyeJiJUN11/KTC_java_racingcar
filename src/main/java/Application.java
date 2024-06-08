362a6b51d51f9c9d2adf07d1ca23f24329db5bd7
HyeJiJUN <jhj910421@gmail.com>
2024-06-08 23:48:19 +0900

    feat: add CarRaceTry

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Application {
@@ -18,13 +19,41 @@ public class Application {
             }
             //아니면 car 객체 생성후 넣음
             carList.add(new Car(name));
+        }
 
-            //시도할 횟수 입력
-            System.out.println("시도할 회수는 몇회인가요?");
-            int tryNum = input.nextInt();
+        //시도할 횟수 입력
+        System.out.println("시도할 회수는 몇회인가요?");
+        int tryNum = input.nextInt();
 
+        System.out.println();
+        System.out.println("실행결과");
+
+        //시도횟수 만큼 반복
+        for(int i=0; i<tryNum; i++){
+            //랜덤돌림
+            CarRaceTry(carList);
+            //결과 프린트
+            CarRacePrint(carList);
             System.out.println();
-            System.out.println("실행결과");
+        }
+    }
+
+    private static void CarRacePrint(List<Car> carList) {
+    }
+
+    /**
+     * 각 car마다 랜덤 숫자가 4이상이면 1칸 전진
+     * @param carList
+     */
+    private static void CarRaceTry(List<Car> carList) {
+        for(Car car : carList){
+            Random random = new Random();
+            if(random.nextInt(9) >= 4){
+                //4이상이면 전진
+                int goTemp = car.getGo();
+                goTemp++;
+                car.setGo(goTemp);
+            }
         }
     }
 }
