70f504205f799bb3eebd26e47675b26aabeed8e5
HyeJiJUN <jhj910421@gmail.com>
2024-06-09 04:35:00 +0900

    test: MakeCarListTry

--- src/test/java/ApplicationTest.java
+++ src/test/java/ApplicationTest.java
@@ -3,12 +3,29 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ApplicationTest {
     @Test
-    void CarRaceTry(){
+    void MakeCarListTry(){
         Application app = new Application();
+        String[] carNameList = {"aaa", "bbb", "ccc"};
+        List<Car> resultCarList = app.MakeCarList(carNameList);
+
+        List<Car> expectCarList = new ArrayList<>();
+        expectCarList.add(new Car("aaa"));
+        expectCarList.add(new Car("bbb"));
+        expectCarList.add(new Car("ccc"));
+
+        for(int i=0; i<3; i++){
+            assertTrue(expectCarList.get(i).getName().equals(resultCarList.get(i).getName())
+                    && expectCarList.get(i).getGo() == 0);
+        }
+    }
+
+    @Test
+    void CarRaceTry(){
         List<Car> carList = new ArrayList<>();
         carList.add(new Car("aaa"));
         carList.add(new Car("bbb"));
@@ -16,7 +33,4 @@ public class ApplicationTest {
         assertTrue(carList.get(0).getGo() == 0 || carList.get(0).getGo() == 1);
         assertTrue(carList.get(1).getGo() == 3 || carList.get(1).getGo() == 4);
     }
-
-//    @Test
-//    void Car
 }
